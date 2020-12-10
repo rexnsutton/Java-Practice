d2842b0274dfbd471f742f5e8dac3cc7de0e92e8
Student Name <rexsutton2@gmail.cogit>
2020-12-10 11:49:12 -0600

    completed first part of arrays exercise

---
+++ src/JavaArrays.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class JavaArrays {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+    }
+
+}
